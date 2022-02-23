d0a4994f19cb6f5638697a5f32113dc80b37fe86
Pablo Coutinho <pablo.coutinho19@gmail.com>
2022-02-23 17:59:47 -0300

    refactors e styles

--- src/components/how-to-play/index.tsx
+++ src/components/how-to-play/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css';
 
 const HowToPlay = () => {
     return (
         <div className="how-to-play">
             <h2>Como jogar?</h2>
-            <p>Regras básicas: <br /> Pedra ganha de Tesoura <br /> Tesoura ganha de Papel <br /> Papel ganha de Pedra </p>
+            <h4>Regras básicas:</h4>
+            <p>Pedra ganha de Tesoura <br /> Tesoura ganha de Papel <br /> Papel ganha de Pedra </p>
             <Link to="/">Voltar ao início</Link>
         </div>
     )
